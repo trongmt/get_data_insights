bbac9dd7719d6024e1a7ed8549f63049e0bb4876
dothaohieu <62645451+dothaohieu@users.noreply.github.com>
2021-02-05 16:51:40 +0700

    cập nhật gần xong

--- code/json_postgres_test.py
+++ code/json_postgres_test.py
@@ -15,6 +15,11 @@ page_id='217328504988428'
 page_token = 'EAAyBEgkHZCbYBANms9s1kPc3Fwrw3fr9OfZCRDIlJT1hQTfhzlidpUt3irjLqd4EjI4F1KYlEbBkHGm1obIJ1iZC7Hf8da9aU7ZAJsOGCPFlDhUKTM32yr6tJmsPmdhFurmipGis6YxHdQYLdEUZBzuITg1Ynzl6C4w3PzxhJfQZDZD'
 graph = fb.GraphAPI(access_token=page_token, version="3.1")
 
+j_table = sqlalchemy.table("fb_data")                  
+    # drop existing records
+    # sql=f"delete from fb_data where end_time between '{from_date}' and '{to_date}'"
+
+find = engine.execute(j_table.delete())
 
 def get_values(data):
     devices = []
@@ -57,14 +62,26 @@ def delete(from_date, to_date):
     return find
 
 if '__name__==__main__':
-    from_date = datetime(2019, 1, 1)
-    to_date = datetime(2019, 1, 10)
+    from_date = datetime(2018, 2, 31)
+    to_date = datetime(2019, 3, 1)
     page_insights = graph.get_connections(
                         id = page_id,
                         connection_name = 'insights',
-                        metric = '''page_total_actions,page_views_total,page_preview_total,page_fan_adds_unique,
-                                    page_impressions_unique,page_post_engagements,page_video_views,
-                                    page_daily_follows_unique''',
+                        metric = '''page_fans,page_fan_adds_unique,page_fan_removes_unique,
+                                    page_engaged_users,page_impressions_unique,page_impressions_organic_unique,
+                                    page_impressions_paid_unique,page_impressions_viral_unique,page_impressions,
+                                    page_impressions_organic,page_impressions_paid,page_impressions_viral,
+                                    page_posts_impressions_unique,page_posts_impressions_organic_unique,page_posts_impressions_paid_unique,
+                                    page_posts_impressions_viral_unique,page_posts_impressions,page_posts_impressions_organic,
+                                    page_posts_impressions_paid,page_posts_impressions_viral,page_consumptions_unique,
+                                    page_consumptions,page_negative_feedback_unique,page_negative_feedback,
+                                    page_places_checkin_total,page_places_checkin_total_unique,page_places_checkin_mobile,
+                                    page_places_checkin_mobile_unique,page_video_views_organic,page_video_views_paid,
+                                    page_video_complete_views_30s_organic,page_video_complete_views_30s_paid,page_video_views,
+                                    page_video_repeat_views,page_video_views_unique,page_video_complete_views_30s,page_video_complete_views_30s_autoplayed,
+                                    page_video_complete_views_30s_click_to_play,page_video_complete_views_30s_repeat_views,page_video_complete_views_30s_unique,
+                                    page_total_actions,page_get_directions_clicks_logged_in_unique,page_call_phone_clicks_logged_in_unique,page_website_clicks_logged_in_unique
+                                ''',
                         period = 'day',
                         since = from_date,
                         until = to_date,
@@ -79,5 +96,4 @@ if '__name__==__main__':
     delete(from_date, to_date)
     flat=flatten_json(dfs)
     data_sql(flat)
-
 
